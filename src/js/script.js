469a07f658858977d40e7434bae3aafafa2c589d
Kamil Niewielski <nevel1906.kn@gmail.com>
2026-04-23 14:09:30 +0100

    rendering products script.js

--- src/js/script.js
+++ src/js/script.js
@@ -54,9 +54,26 @@ const select = {
 class Product{
   constructor(id, data){
     const thisProduct = this;
+
     thisProduct.id = id;
     thisProduct.data = data;
+
+    thisProduct.renderInMenu();
+
     console.log('newProduct:', thisProduct);
+  }
+
+  renderInMenu(){
+    const thisProduct = this;
+
+    /* generate HTML based on template */
+    const generatedHTML = templates.menuProduct(thisProduct.data);
+    /* create element using utils.createElementFromHTML */
+    thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+    /* find menu container */
+    const menuContainer = document.querySelector(select.containerOf.menu);
+    /* add element to menu */
+    menuContainer.appendChild(thisProduct.element);
   }
 }
 
